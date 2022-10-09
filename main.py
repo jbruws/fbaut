c7b054629c495be6de7d030144627d2dc67f94a9
br-netw <vanmain.22@gmail.com>
2022-10-09 14:08:25 +0300

    Переделал программу под ООП

--- main.py
+++ main.py
@@ -4,204 +4,205 @@ import json
 
 # TODO:
 # - КАК Я БУДУ ПОДАВАТЬ КРОНУ ДАННЫЕ ДЛЯ ВХОДА В ГИТХАБ????????????
+# - Сделать так, чтобы нормально работал пуш через ssh
 
-rc_dir = None
-rc_name = None
-config_dir = None
 MAIN_PATH = os.path.dirname(__file__) + "/" + os.path.basename(__file__)
 USERNAME = os.getlogin()
 
-def help(*args):
-    print("""
-        -- help                    - вывести список команд\n
-        -- cache [список папок]    - добавить файлы/папки в .conbatrc\n
-        -- uncache [список папок]  - убрать файлы/папки из .conbatrc\n
-        -- set_rc /путь/к/папке    - задать рабочую директорию\n
-        -- backup                  - произвести бэкап файлов из .conbatrc\n
-        -- schedule [d/w/m]        - производить авто-бэкап ежедневно/еженедельно/ежемесячно\n
-        -- show_rc                 - вывести содержимое .conbatrc в консоль\n
-        -- git_init [ссылка на гх] - создать локальный репозиторий git и связать его с репозиторием github\n
-        -- quit                    - выйти
-    """)
+class ConfigManager:
+    global MAIN_PATH
+    global USERNAME
+    
+    def __init__(self):
+        self.rc_dir = None
+        self.rc_name = None
+        self.config_dir = None
 
-def set_rc(*args):
-    global rc_dir
-    global rc_name
-    global config_dir
-    rc_dir = args[0][0]
-    os.chdir(rc_dir)
-    config_dir = rc_dir + "/configs"
-    rc_name = ".conbatrc"
+    def help(self, *args):
+        print("""
+            -- help                    - вывести список команд
+            -- cache [список папок]    - добавить файлы/папки в .conbatrc
+            -- uncache [список папок]  - убрать файлы/папки из .conbatrc
+            -- set_rc /путь/к/папке    - задать рабочую директорию
+            -- backup                  - произвести бэкап файлов из .conbatrc
+            -- schedule [d/w/m]        - производить авто-бэкап ежедневно/еженедельно/ежемесячно
+            -- show_rc                 - вывести содержимое .conbatrc в консоль
+            -- git_init [ссылка на гх] - создать локальный репозиторий git и связать его с репозиторием github
+            -- quit                    - выйти
+        """)
 
-def show_rc(*args):
-    try:
-        contents = json.load(open(rc_name, "r"))
-    except FileNotFoundError:
-        print("ОШИБКА: .conbatrc не найден.")
+    def set_rc(self, *args):
+        self.rc_dir = args[0][0]
+        if not os.path.isdir(self.rc_dir):
+            print("ОШИБКА: не является директорией")
+            return 1
+        os.chdir(self.rc_dir)
+        self.config_dir = self.rc_dir + "/configs"
+        self.rc_name = ".conbatrc"
 
-    for i in list(contents.keys()):
-        if contents[i] == "f":
-            print("file: " + i)
+    def show_rc(self, *args):
+        try:
+            contents = json.load(open(self.rc_name, "r"))
+        except FileNotFoundError:
+            print("ОШИБКА: .conbatrc не найден.")
+            return 1
+
+        for i in list(contents.keys()):
+            if contents[i] == "f":
+                print("file: " + i)
+            else:
+                print("dir:  " + i)
+
+    def cache(self, *args):
+        filenames = args[0]
+        # Если rc-файл пуст, присваиваем переменной пустой словарь
+        if not os.path.isfile(self.rc_name):
+            rc_contents = {}
         else:
-            print("dir:  " + i)
+            f = open(self.rc_name, "r")
+            rc_contents = json.loads(f.read())
+            f.close()
 
-def cache(*args):
-    filenames = args[0]
-    # Если rc-файл пуст, присваиваем переменной пустой словарь
-    if not os.path.isfile(rc_name):
-        rc_contents = {}
-    else:
-        f = open(rc_name, "r")
-        rc_contents = json.loads(f.read())
+        # Записываем, учитывая тип  (файл/директория)
+        for i in filenames:
+            if os.path.isfile(i):
+                rc_contents[i] = "f"
+            elif os.path.isdir(i):
+                rc_contents[i] = "d"
+            else:
+                print(i + " - несуществующий файл/директория. Пропускаем...")
+       
+        f = open(self.rc_name, "w")
+        f.write(json.dumps(rc_contents, indent=4))
         f.close()
 
-    # Записываем, учитывая тип  (файл/директория)
-    for i in filenames:
-        if os.path.isfile(i):
-            rc_contents[i] = "f"
-        elif os.path.isdir(i):
-            rc_contents[i] = "d"
+    def uncache(self, *args):
+        filenames = args[0]
+        if not os.path.isfile(self.rc_name):
+            print("ОШИБКА: файла .conbatrc не существует.")
+            return 1
+        f = open(self.rc_name, "r")
+        rc_contents = json.loads(f.read())
+        f.close()
+        for i in filenames:
+            if i in rc_contents.keys():
+                del(rc_contents[i])
+        
+        f = open(self.rc_name, "w")
+        f.write(json.dumps(rc_contents, indent=4))
+        f.close()
+
+    def backup(self, *args):
+        custom_commit_msg = True
+        if self.rc_dir == None: # если вызывается cron'ом
+            set_rc(args[0][0])
+            custom_commit_msg = False
+
+        file_list = json.load(open(self.rc_name, "r"))
+        if not os.path.isdir(self.config_dir):
+            os.mkdir(self.config_dir)
         else:
-            print(i + " - несуществующий файл/директория. Пропускаем...")
-   
-    f = open(rc_name, "w")
-    f.write(json.dumps(rc_contents, indent=4))
-    f.close()
+            # rm не стирает скрытые директории, так что .git остаётся
+            os.system("rm -r " + self.config_dir + "/*") 
 
-def uncache(*args):
-    filenames = args[0]
-    if not os.path.isfile(rc_name):
-        print("ОШИБКА: нечего удалять")
-        return -1
-    f = open(rc_name, "r")
-    rc_contents = json.loads(f.read())
-    f.close()
-    for i in filenames:
-        if i in rc_contents.keys():
-            del(rc_contents[i])
-    
-    f = open(rc_name, "w")
-    f.write(json.dumps(rc_contents, indent=4))
-    f.close()
+        for i in file_list.keys(): 
+            os.system("cp -r --parents " + i + " " + self.config_dir)
 
-def backup(*args):
-    global rc_dir
-    global rc_name
-    global config_dir
-    
-    custom_commit_msg = True
-    if rc_dir == None: # если вызывается cron'ом
-        set_rc(args[0][0])
-        custom_commit_msg = False
+        # если есть git-репозиторий
+        if os.path.isdir(self.config_dir + "/.git"):
+            os.chdir(self.config_dir)
+            os.system('git add -A')
 
-    file_list = json.load(open(rc_name, "r"))
-    if not os.path.isdir(config_dir):
-        os.mkdir(config_dir)
-    else:
-        # трём все в папке бэкапов, кроме .git (и .gitignore?)
-        os.system("rm -r !(.git*) " + config_dir) 
+            # используем пользовательское название коммита, если оно есть
+            if args != ([],) and custom_commit_msg:
+                commit_msg = " ".join(args[0][:])
+            else:
+                try:
+                    commit_count = int(os.popen("git rev-list --count HEAD").read())
+                except ValueError:
+                    commit_count = 0
+                commit_msg = "Копия #{}".format(commit_count + 1)
+                
+            os.system('git commit -m "{}"'.format(commit_msg))
+            os.system('git push -u origin master')
+            os.chdir(self.rc_dir)
 
-    for i in file_list.keys(): 
-        os.system("cp -r --parents " + i + " " + config_dir)
+    def git_init(self, *args):
+        origin_link = args[0][0]
+        if os.path.isdir(self.config_dir + "/.git"):
+            print("ВНИМАНИЕ: репозиторий Git уже существует в рабочей директории.")
+            return 1
 
-    # если есть git-репозиторий
-    if os.path.isdir(config_dir + "/.git"):
-        os.chdir(config_dir)
-        os.system('git add -A')
+        if not os.path.isdir(self.config_dir):
+            os.mkdir(self.config_dir)
+        
+        os.chdir(self.config_dir)
+        os.system("git init")
+        os.system("git remote add origin " + origin_link)
+        os.chdir(self.rc_dir)
 
-        # используем пользовательское название коммита, если оно есть
-        if args != ([],) and custom_commit_msg: # TODO: сделать по-человечески
-            commit_msg = " ".join(args[0][:])
-        else:
-            try:
-                commit_count = int(os.popen("git rev-list --count HEAD").read())
-            except ValueError: # По сути, этого не должно происходить
-                commit_count = 0
-            commit_msg = "Копия #{}".format(commit_count + 1)
-            
-        os.system('git commit -a -m "{}"'.format(commit_msg))
-        os.system('git push')
-        os.chdir(rc_dir)
+    # Всё ещё не работает, но концепт понятен. Допилю в Qt-версии
+    def schedule(self, *args):
+        job_type = args[0][0]
+        cronjob_types = {"d": "@daily", "w": "@weekly", "m": "@monthly"}
+        cron_status = os.popen("systemctl --no-pager status cronie").read()
 
-def git_init(*args):
-    origin_link = args[0][0]
-    if os.path.isdir(config_dir + "/.git"):
-        print("ВНИМАНИЕ: репозиторий Git уже существует в рабочей директории.")
-        return -1
+        # Включаем cron при необходимости
+        if "disabled; preset: disabled" in cron_status or "inactive (dead)" in cron_status:
+            print("cron выключен. Запускаем сервис...")
+            os.system("sudo systemctl enable cronie")
+            os.system("sudo systemctl start cronie")
+        
+        # Создаём файл (при необходимости) и читаем
+        crontab_path = "/var/spool/cron/{}".format(USERNAME)
+        if not os.path.isfile(crontab_path):
+            print("Файла crontab для пользователя {} не существует. Создаём...".format(USERNAME))
+            os.system("sudo touch {}".format(crontab_path))
+            os.system("sudo chown {} {}".format(USERNAME, crontab_path))
+        f = open(crontab_path, "r")
+        crontab_contents = f.readlines()
+        f.close()
 
-    if not os.path.isdir(config_dir):
-        os.mkdir(config_dir)
-    
-    os.chdir(config_dir)
-    os.system("git init")
-    os.system("git remote add origin " + origin_link)
-    os.system('git commit -a -m "Initial commit"')
-    os.system('git push -u origin master')
-    os.chdir(rc_dir)
+        conbat_cronjob = cronjob_types[job_type] + " " + MAIN_PATH + " backup " + self.rc_dir + " # conbat job\n"
+        conbat_cronjob_id = 0
 
-# Всё ещё не работает, но концепт понятен. Допилю в Qt-версии
-def schedule(*args):
-    job_type = args[0][0]
-    cronjob_types = {"d": "@daily", "w": "@weekly", "m": "@monthly"}
-    cron_status = os.popen("systemctl --no-pager status cronie").read()
+        # удаляем старую запись и добавляем новую на её место
+        for i in range(len(crontab_contents)-1):
+            if "# conbat job" in crontab_contents[i]:
+                crontab_contents.pop(i)
+                conbat_cronjob_id = i
+                break
 
-    # Включаем cron при необходимости
-    if "disabled; preset: disabled" in cron_status or "inactive (dead)" in cron_status:
-        print("cron выключен. Запускаем сервис...")
-        os.system("sudo systemctl enable cronie")
-        os.system("sudo systemctl start cronie")
-    
-    # Создаём файл (при необходимости) и читаем
-    crontab_path = "/var/spool/cron/{}".format(USERNAME)
-    if not os.path.isfile(crontab_path):
-        print("Файла crontab для пользователя {} не существует. Создаём...".format(USERNAME))
-        os.system("sudo touch {}".format(crontab_path))
-        os.system("sudo chown {} {}".format(USERNAME, crontab_path))
-    f = open(crontab_path, "r")
-    crontab_contents = f.readlines()
-    f.close()
+        crontab_contents.insert(conbat_cronjob_id, conbat_cronjob)
 
-    conbat_cronjob = cronjob_types[job_type] + " " + MAIN_PATH + " backup " + rc_dir + " # conbat job\n"
-    conbat_cronjob_id = 0
+        f = open(crontab_path, "w")
+        for i in crontab_contents:
+            f.write(i)
+        f.close()
 
-    # удаляем старую запись и добавляем новую на её место
-    for i in range(len(crontab_contents)-1):
-        if "# conbat job" in crontab_contents[i]:
-            crontab_contents.pop(i)
-            conbat_cronjob_id = i
-            break
+    def quit(self, *args):
+        sys.exit()
 
-    crontab_contents.insert(conbat_cronjob_id, conbat_cronjob)
+    def preprocess(self, arg_list):
+        for i in range(len(arg_list)):
+            if "~" in arg_list[i]:
+                arg_list[i] = arg_list[i].replace("~", "/home/{}".format(USERNAME))
+        return arg_list
 
-    f = open(crontab_path, "w")
-    for i in crontab_contents:
-        f.write(i)
-    f.close()
-
-def quit(*args):
-    sys.exit()
-
-def preprocess(arg_list):
-    for i in range(len(arg_list)):
-        if "~" in arg_list[i]:
-            arg_list[i] = arg_list[i].replace("~", "/home/{}".format(USERNAME))
-    return arg_list
-
-def main():
+if __name__ == "__main__":
     args = sys.argv
+    manager = ConfigManager()
+    
     if len(args) > 1: # Если указана функция для запуска...
-        globals()[args[1]](args[2:]) # ...запускаем только её
-        return 0
+        eval("manager.{}({})".format(args[1], str(args[2:]))) # ...запускаем только её
+        quit()   
 
     while True:
         command = input("> ").split(" ")
-        command = preprocess(command)
-        if command[0] not in ["set_rc", "help", "quit"] and rc_dir == None:
+        command = manager.preprocess(command)
+        if command[0] not in ["set_rc", "help", "quit"] and manager.rc_dir == None:
             print("ВНИМАНИЕ: не установлена директория с .conbatrc; запустите set_rc [путь к директории]")
-        elif command[0] not in globals():
+        elif command[0] not in globals()['ConfigManager'].__dict__:
             print("ВНИМАНИЕ: неизвестная команда")
         else:
-            globals()[command[0]](command[1:])
-
-if __name__ == "__main__":
-    main()
+            eval("manager.{}({})".format(command[0], str(command[1:])))
